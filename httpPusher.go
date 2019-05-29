647dd19b77aaadafef466e99f2ced5adc30fbfa3
Trond Kandal <Trond.Kandal@ntnu.no>
2019-05-30 00:22:48 +0200

    Added prometheus metric

--- httpPusher.go
+++ httpPusher.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"go.uber.org/zap"
 	"io"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -16,20 +18,38 @@ import (
 
 // HTTPPusher uses HTTP to push to endpoint
 type HTTPPusher struct {
-	URL     string
-	User    string
-	Pass    string
-	Method  string
-	Logger  *zap.SugaredLogger
-	Headers map[string]string
-	Timeout time.Duration
-	client  *http.Client
+	URL          string
+	User         string
+	Pass         string
+	Method       string
+	Logger       *zap.SugaredLogger
+	Headers      map[string]string
+	Timeout      time.Duration
+	client       *http.Client
+	HistogramVec *prometheus.HistogramVec
 }
 
 // Push pushes cards to destination endpoint
 func (hp *HTTPPusher) Push(r io.Reader) (io.Reader, error) {
+	u, err := url.Parse(hp.URL)
+	if err != nil {
+		hp.Logger.Warnw("URL not parseable", "error", err.Error())
+	}
+
+	status := http.StatusOK
+	defer func(s time.Time) {
+		if hp.HistogramVec != nil && u != nil {
+			hp.HistogramVec.With(prometheus.Labels{
+				"route":  u.Path,
+				"method": hp.Method,
+				"status": fmt.Sprintf("%d", status),
+			}).Observe(float64(time.Now().Sub(s).Nanoseconds() / int64(time.Millisecond)))
+		}
+	}(time.Now())
+
 	req, err := http.NewRequest(hp.Method, hp.URL, r)
 	if err != nil {
+		status = http.StatusInternalServerError
 		hp.Logger.Errorw("new request failed", "error", err.Error())
 		return nil, err
 	}
@@ -53,6 +73,7 @@ func (hp *HTTPPusher) Push(r io.Reader) (io.Reader, error) {
 
 	resp, err := hp.client.Do(req.WithContext(ctx))
 	if err != nil {
+		status = http.StatusInternalServerError
 		hp.Logger.Errorw("request failed", "error", err.Error())
 		return nil, err
 	}
@@ -61,11 +82,13 @@ func (hp *HTTPPusher) Push(r io.Reader) (io.Reader, error) {
 	respBuf := &bytes.Buffer{}
 	_, err = respBuf.ReadFrom(resp.Body)
 	if err != nil {
+		status = http.StatusInternalServerError
 		hp.Logger.Errorw("read response failed", "error", err.Error())
 		return nil, err
 	}
 
 	if resp.StatusCode >= http.StatusMultipleChoices {
+		status = resp.StatusCode
 		hp.Logger.Errorf("%s %s returned status-code %d", hp.Method, hp.URL, resp.StatusCode)
 		hp.Logger.Error(respBuf.String())
 		return nil, fmt.Errorf("%s %s returned status-code %d", hp.Method, hp.URL, resp.StatusCode)
